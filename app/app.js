e150e94ac875c6d5d835ee782c52aba583b4c4a6
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-04-08 08:45:53 -0500

    Added some comments within app.js

--- app/app.js
+++ app/app.js
@@ -4,6 +4,10 @@
 //Using angular-ui-route and angular-ui-bootstrap
 var app = angular.module("ClassWebsite", ['ui.router', 'ui.bootstrap'])
 
+/*
+	This gets updated on app.run at the bottom of the file
+	There is a $s.$watch() waiting for this on HomeCtrl.js
+*/
 .service('StudentProfiles', function() {
 	this.studentArray = [];
 })
@@ -41,10 +45,17 @@ var app = angular.module("ClassWebsite", ['ui.router', 'ui.bootstrap'])
 	
 })
 
+
+/*
+	As soon s the page runs, we make a call to our JSON file representing all students.
+	This JSON array gets assigned to StudentProfiles.studentArray to be iterated over 
+	There is a $s.$watch() that waits for this to be completed within HomeCtrl.js
+	The array gets iterated over within StudentProfiles.html
+*/
 .run(($http, StudentProfiles) => {
 
 	$http.get('../StudentProfiles.json').then(
-			(studentProfiles) => StudentProfiles.studentArray = studentProfiles.data
+			(studentProfilesJson) => StudentProfiles.studentArray = studentProfilesJson.data
 		);
 
 });
